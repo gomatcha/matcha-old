e877958bf47e8a6367ed1423394304339fdbdfc7
Kevin Dang <overcyn@gmail.com>
2017-03-21 10:18:14 -0700

    Geometry primitives

--- main.go
+++ main.go
@@ -5,3 +5,29 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, 世界")
 }
+
+type View struct {
+	Frame    Rect
+	Bounds   Rect
+	Layouter Layouter
+}
+
+type Rect struct {
+	Size  Size
+	Point Point
+}
+
+type Size struct {
+	Width  float64
+	Height float64
+}
+
+type Point struct {
+	Width  float64
+	Height float64
+}
+
+type Layouter interface {
+	NeedsLayout() bool
+	Layout(minSize Size, maxSize Size) Size
+}
