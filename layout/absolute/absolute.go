032ae88f62a0ccbddfa2f8bccf47763acd073b9d
Kevin Dang <overcyn@gmail.com>
2017-06-11 00:41:55 -0700

    Add() for layout.absolute

--- layout/absolute/absolute.go
+++ layout/absolute/absolute.go
@@ -3,11 +3,29 @@ package absolute
 import (
 	"github.com/overcyn/mochi"
 	"github.com/overcyn/mochi/layout"
+	"github.com/overcyn/mochi/view"
 )
 
 type Layout struct {
-	Guide       layout.Guide
-	ChildGuides map[mochi.Id]layout.Guide
+	guide       layout.Guide
+	childGuides map[mochi.Id]layout.Guide
+	views       []view.View
+}
+
+func (l *Layout) Guide() layout.Guide {
+	return l.guide
+}
+
+func (l *Layout) SetGuide(g layout.Guide) {
+	l.guide = g
+}
+
+func (l *Layout) Add(v view.View, g layout.Guide) {
+	if l.childGuides == nil {
+		l.childGuides = map[Mochi.Id]layout.Guide{}
+	}
+	l.childGuides[v.Id()] = g
+	l.views = append(l.Views, v)
 }
 
 func (l *Layout) Layout(ctx *layout.Context) (layout.Guide, map[mochi.Id]layout.Guide) {
